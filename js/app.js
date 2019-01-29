6bf9ccafd6dc4db5174a8a31895b60205937abc9
dusanpopov <aristocut90@gmail.com>
2019-01-29 16:51:29 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+const searchBtn = document.querySelector(".search--container__button");
+const search = document.querySelector(".search--container__input");
+const tip = document.querySelector(".tip");
+
+
+searchBtn.addEventListener("click", () => {
+
+    search.classList.toggle("search-style");
+    tip.classList.remove("tip-style");
+    search.focus();
+    search.value = "";
+
+    let charNumber = 0;
+    let placeholderMessage = "Enter a search term";
+    let speed = 100;
+    let inputMessage = "";
+
+    function typeWriterEffect() {
+
+        if (charNumber < placeholderMessage.length) {
+
+            inputMessage += `${placeholderMessage.charAt(charNumber)}`;
+            search.setAttribute("placeholder", inputMessage);
+            charNumber += 1;
+            setTimeout(typeWriterEffect, speed);
+        }
+    }
+
+    typeWriterEffect();
+
+});
+
+search.addEventListener("keydown", () => {
+    tip.classList.add("tip-style");
+});
